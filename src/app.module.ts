6914bd13f31c914051cd9e2ff78a97396bbbc56b
xiangshilin <295755973@qq.com>
2022-05-11 14:17:38 +0800

    fix: API文档

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,15 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { BannerModule } from './banner/banner.module';
 import { ComponentModule } from './component/component.module';
 import { PluginModule } from './plugin/plugin.module';
 import { ProjectModule } from './project/project.module';
 import { ReleaseModule } from './release/release.module';
 import { TemplateModule } from './template/template.module';
-
-// 子模块加载
 import { UserModule } from './user/user.module';
 import { BingModule } from './bing/bing.module';
 
 @Module({
   imports: [
-    // 加载连接数据库
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql', // 数据库类型
-    //   host: '152.32.211.215', // 数据库ip地址
-    //   port: 3306, // 端口
-    //   username: 'mumu', // 登录名
-    //   password: 'W3LWJXyMEKCNzHTy', // 密码
-    //   database: 'mumu', // 数据库名称
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'], // 扫描本项目中.entity.ts或者.entity.js的文件
-    //   synchronize: true, // 定义数据库表结构与实体类字段同步(这里一旦数据库少了字段就会自动加入,根据需要来使用)
-    //   timezone: '+08:00', // 东八区
-    // }),
-    // 加载子模块
     UserModule,
     BannerModule,
     ComponentModule,
@@ -33,7 +17,7 @@ import { BingModule } from './bing/bing.module';
     ProjectModule,
     ReleaseModule,
     TemplateModule,
-    BingModule
+    BingModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { createConnection } from "typeorm";
+import { createConnection } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // createConnection方法会自动读取来自ormconfig文件或环境变量中的连接选项
   await createConnection();
-  app.enableCors({ origin: ['http://localhost:3000', 'https://mumu-page.github.io/'], credentials: true });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://mumu-page.github.io'],
+    credentials: true,
+  });
   // swagger配置
   const options = new DocumentBuilder()
-    // .setTitle('')
-    // .setDescription('')
-    // .setVersion('1.0')
-    // .addTag('cats')
+    .setTitle('页面搭建(mumu-page)后端API文档')
+    .setDescription('页面搭建服务端(mumu-server)')
+    .setVersion('1.0')
+    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
   await app.listen(7001);
 }
+
 bootstrap();
